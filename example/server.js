322b13eac06ff9002b954ad096ee5e713e38a2a4
Johnny Reina <johnny.r.reina@gmail.com>
2020-05-16 12:44:29 -0500

    🚀

--- example/server.js
+++ example/server.js
@@ -1,7 +1,7 @@
 const server = require("express")();
 const { troll } = require("../src");
 
-server.use(troll);
+server.use(troll());
 
 server.get("/", function (req, res) {
   res.status(200).send("<h1>Hello, world!</h1>");
